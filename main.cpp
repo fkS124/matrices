569d299cf3a2227be0e6191774e1294c4367376e
fkS124 <theophile.aumont@gmail.com>
2022-05-27 17:48:15 +0200

    Cleanings and fixes.

--- main.cpp
+++ main.cpp
@@ -1,106 +1,116 @@
-#include <stdlib.h>
 #include <iostream>
-#include <stdio.h>
+#include <vector>
+#include <string>
+
 #include "lib/matrix_object.h"
 #include "lib/cli_operations.h"
-#include <vector>
+
+using std::string, std::stoi;
+using word_list = std::vector<string>;
+using matrix_list = std::vector<Matrix>;
+
+const int commandNumber = 9;
+string commandList[commandNumber] = {"input", "sum", "subtract", "multk", "product", "show", "det", "inv", "exit"};
+int nArgs[commandNumber] = {2, 2, 2, 2, 2, 1, 1, 1, 0};
+matrix_list matrices;
+
+word_list scanInput(const string &input);
+string getFunc(word_list &command);
+int execFunc(word_list &command);
 
 int main(int argc, char** argv) 
 {
-    // create the array of matrices
-    std::vector<Matrix> matrices;
+    // create the string that will be used as an input
+    string input;
 
-    std::string input;
+    // program's main loop
     do {
         std::cout << ">>> ";
-        std::cin >> input;
-        if (input == "input") {
-            int n_row, n_col;
-            std::cout << "Number of rows : ";
-            std::cin >> n_row;
-            std::cout << "Number of columns : ";
-            std::cin >> n_col;
+        std::getline(std::cin, input);
 
-            Matrix new_matrix(n_row, n_col);
-            new_matrix.inputMatrix();
-            matrices.push_back(new_matrix);
+        if (input == "exit") 
+            break;
+        
+        else if (input != "") {
+            word_list command = scanInput(input);
+            
+            std::cout << getFunc(command) << std::endl;
         }
-        else if (input == "sum") {
-            int index1, index2;
-            std::cout << "Enter the index of the first matrix of the sum : ";
-            std::cin >> index1;
-            std::cout << "Enter the index of the second matrix of the sum : ";
-            std::cin >> index2;
-            if (0 <= index1 && index1 < matrices.size() || 0 <= index2 && index2 < matrices.size())
-                addMatrices(matrices[index1], matrices[index2], false);
-            else
-                std::cerr << "Error: Passed indexes are incorrect." << std::endl;
-        }
-        else if (input == "subtract") {
-            int index1, index2;
-            std::cout << "Enter the index of the first matrix of the substraction : ";
-            std::cin >> index1;
-            std::cout << "Enter the index of the second matrix of the substraction : ";
-            std::cin >> index2;
-            if (0 <= index1 && index1 < matrices.size() || 0 <= index2 && index2 < matrices.size())
-                subMatrices(matrices[index1], matrices[index2], false);
-            else
-                std::cerr << "Error: Passed indexes are incorrect." << std::endl;
-        }
-        else if (input == "multk") {
-            int index, k;
-            std::cout << "Enter the index of the matrix to multiply : ";
-            std::cin >> index;
-            std::cout << "Enter the number to multiply the matrix by : ";
-            std::cin >> k;
-            if (0 <= index && index < matrices.size())
-                multkMatrix(matrices[index], k, false);
-            else
-                std::cerr << "Error: Passed index is incorrect." << std::endl;
-        }
-        else if (input == "product") {
-            int index1, index2;
-            std::cout << "Enter the index of the first matrix of the product : ";
-            std::cin >> index1;
-            std::cout << "Enter the index of the second matrix of the product : ";
-            std::cin >> index2;
-            if (0 <= index1 && index1 < matrices.size() || 0 <= index2 && index2 < matrices.size())
-                prodMatrices(matrices[index1], matrices[index2], false);
-            else
-                std::cerr << "Error: Passed indexes are incorrect." << std::endl;
-        }
-        else if (input == "show") {
-            int index;
-            std::cout << "Enter the index of the matrix you want to show : ";
-            std::cin >> index;
-            if (0 <= index && index < matrices.size())
-                matrices[index].showMatrix();
-            else 
-                std::cerr << "Error: Passed index is incorrect." << std::endl;
-        }
-        else if (input == "det") {
-            int index;
-            std::cout << "Enter the index of the matrix you want to calculate the determinant of : ";
-            std::cin >> index;
-            if (matrices[index].getRowNumber() != matrices[index].getColumnNumber())
-                std::cerr << "Error: passed matrix is invalid." << std::endl;
-            else 
-                determinantMatrix(matrices[index]);
-        }
-        else if (input == "inv") {
-            int index;
-            std::cout << "Enter the index of the matrix you want to show : ";
-            std::cin >> index;
-            if (0 <= index && index < matrices.size())
-                invMatrix(matrices[index], false);
-            else
-                std::cerr << "Error: passed index is incorrect." << std::endl;
-        }
-        else {
-            if (input != "exit")
-                std::cout << input << " is not a regognized function. Type 'help' to get all the commands." << std::endl;
-        }
+        
     } while(input != "exit");
+
+    return 0;
+}
+
+
+word_list scanInput(const string &input) 
+{
+    // function to execute an inputed command
+    string temp = "";
+    word_list words;
+
+    for (int i = 0; i < input.length(); i++) {
+        if (input[i] == ' ') {
+            words.push_back(temp);
+            temp = "";
+        }
+        else 
+            temp.push_back(input[i]);
+    }
+    words.push_back(temp);
+
+    return words;
+}
+
+
+string getFunc(word_list &command) {
+    for (int i = 0; i < commandNumber; i++) {
+        if (commandList[i] == command[0] && command.size() == nArgs[i]+1) {
+            int returned = execFunc(command);
+            if (returned == 0)
+                return " ";
+            else if (returned == -1)
+                return "Error: index error.";
+            else    
+                return "Error: wrong arguments passed.";
+        }
+    }
+
+    return "Error : unrecognized command.";
+}
+
+
+int execFunc(word_list &command) {
+    string mainCommand = command[0];
+
+    if (mainCommand == "input") {
+        int d1 = stoi(command[1]), d2 = stoi(command[2]);
+        if (d1 <= 0 || d2 <= 0) return -1;
+        Matrix new_matrix(d1, d2);
+        new_matrix.inputMatrix();
+        matrices.push_back(new_matrix);
+    }
+    else if (mainCommand == "sum" || mainCommand == "subtract" || mainCommand == "product" || mainCommand == "multk") {
+        int idx1 = stoi(command[1]), idx2 = stoi(command[2]);
+        if (!(0 <= idx1 && idx1 < matrices.size() || 0 <= idx2 && idx2 < matrices.size() )) return -1;
+        if (mainCommand == "sum")
+            addMatrices(matrices[idx1], matrices[idx2], false);
+        else if (mainCommand == "subtract")
+            subMatrices(matrices[idx1], matrices[idx2], false);
+        else if (mainCommand == "product")
+            prodMatrices(matrices[idx1], matrices[idx2], false);
+        else if (mainCommand == "multk")
+            multkMatrix(matrices[idx1], idx2, false);
+    }
+    else if (mainCommand == "show" || mainCommand == "inv" || mainCommand == "det") {
+        int idx1 = stoi(command[1]);
+        if (!(0 <= idx1 && idx1 < matrices.size())) return -1;
+        if (mainCommand == "show")
+            matrices[idx1].showMatrix();
+        else if (mainCommand == "inv")
+            invMatrix(matrices[idx1], false);
+        else if (mainCommand == "det");
+    }
 
     return 0;
 }
